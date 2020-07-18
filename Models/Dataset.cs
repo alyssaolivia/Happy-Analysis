93e44a2798cd13e4ecadf03521ce9764fbc81481
Alyssa Jordan <alyssaolivia_j@outlook.com>
2020-07-18 15:26:05 -0400

    Create Dataset.cs

---
+++ Models/Dataset.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Happy_Analysis.Models
+{
+    public class Dataset
+    {
+        public string LocationURL { get; set; }
+    }
+}
